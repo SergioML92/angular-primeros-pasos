5dd687433c0349b295d415ad3bee65cc580d99e1
sergio.mohedano <s.mohedano92@gmail.com>
2023-08-24 21:04:12 +0200

    Primer commit

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+import { ListComponent } from './components/list/list.component';
+import { MainPageComponent } from './pages/main-page.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    MainPageComponent,
+    ListComponent,
+    AddCharacterComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    MainPageComponent
+  ]
+})
+export class DbzModule { }
